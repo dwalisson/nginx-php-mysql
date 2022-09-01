c32d447ac133145320f51259aa7d849a1891eb6b
Diego Walisson <diegowalisson@gmail.com>
2022-09-01 20:21:22 -0300

    added composer to php service

--- app/public/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Connect to a MySQL database using driver invocation */
-$dsn = 'mysql:dbname=new_app;host=localhost';
-$user = 'new_app';
-$password = 'new_app';
-
-$dbh = new PDO($dsn, $user, $password);
